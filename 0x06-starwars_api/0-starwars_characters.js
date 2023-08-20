446068cffb49c4cb6134ff8e827653a484384f5f
KayongoYongo <sknsammie10@gmail.com>
2023-08-20 20:52:27 +0000

    update

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,56 +1,25 @@
 #!/usr/bin/node
-
 const request = require('request');
+const API_URL = 'https://swapi-api.hbtn.io/api';
 
-function getMovieCharacters (movieId) {
-  const url = `https://swapi.dev/api/films/${movieId}/`;
-
-  return new Promise((resolve, reject) => {
-    request(url, (error, response, body) => {
-      if (error) {
-        console.error('Error fetching movie data:', error.message);
-        reject(new Error('Failed to fetch movie data'));
-      } else if (response.statusCode === 200) {
-        const filmData = JSON.parse(body);
-        const characterUrls = filmData.characters;
-        const characterPromises = characterUrls.map(charUrl => {
-          return new Promise((resolveChar, rejectChar) => {
-            request(charUrl, (charError, charResponse, charBody) => {
-              if (charError) {
-                rejectChar(new Error('Failed to fetch character data'));
-              } else {
-                const charData = JSON.parse(charBody);
-                resolveChar(charData.name);
-              }
-            });
-          });
+if (process.argv.length > 2) {
+  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
+    if (err) {
+      console.log(err);
+    }
+    const charactersURL = JSON.parse(body).characters;
+    const charactersName = charactersURL.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (promiseErr, __, charactersReqBody) => {
+          if (promiseErr) {
+            reject(promiseErr);
+          }
+          resolve(JSON.parse(charactersReqBody).name);
         });
+      }));
 
-        Promise.all(characterPromises)
-          .then(characterNames => {
-            resolve(characterNames);
-          })
-          .catch(err => {
-            console.error('Error fetching character data:', err.message);
-            reject(new Error('Failed to fetch character data'));
-          });
-      } else {
-        console.error('Error fetching movie data. Status code:', response.statusCode);
-        reject(new Error('Failed to fetch movie data'));
-      }
-    });
+    Promise.all(charactersName)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErr => console.log(allErr));
   });
 }
-
-if (process.argv.length < 3) {
-  console.log('Usage: node script_name.js movie_id');
-  process.exit(1);
-}
-
-const movieId = process.argv[2];
-getMovieCharacters(movieId)
-  .then(characterNames => {
-    characterNames.forEach(name => {
-      console.log(name);
-    });
-  });
