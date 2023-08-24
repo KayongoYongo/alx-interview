0624a176256f0b581710085bde0ed81f2f7e927f
KayongoYongo <sknsammie10@gmail.com>
2023-08-24 01:04:18 +0000

    make change pycodestyle

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
+"""This function finds the change given the coins
+and the total"""
 
 def makeChange(coins, total):
+    """This function finds the change.
+
+    Args:
+        coins: A list of the coins
+        total: The total to be reached
+
+    Return:
+        If total is 0, returns 0
+        if no change is found, returns -1
+        otherwise, it returns the change
+    """
     change = 0
     coins = sorted(coins, reverse=True)
 
@@ -13,5 +26,4 @@ def makeChange(coins, total):
 
     if total == 0:
         return change
-    else:
-        return -1
+    return -1
